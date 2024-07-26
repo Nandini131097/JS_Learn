7c7ac4b7fe746c40a895f55ad526c8e6f510c25e
Nandini131097 <lamgharenandini@gmail.com>
2024-07-26 12:06:55 +0000

    maths functions learn

--- 01_Basic/mathcal.js
+++ 01_Basic/mathcal.js
@@ -10,6 +10,10 @@
 
 // ------------------------------------------------Math----------------------------------------
 console.log(Math.floor(Math.random()* 10)+1);
+console.log(Math.abs(-4));
+console.log(Math.ceil(4.9));
+console.log(Math.floor(4.55));
+console.log(Math.round(5.78));
 const min=10
 const max=20
 console.log(Math.floor(Math.random()* (max-min +1))+min);
