46aacbda954a992e9f8d8f0f264edb4e65cb3f9d
Nandini131097 <lamgharenandini@gmail.com>
2024-07-26 12:03:32 +0000

    maths functions learn

---
+++ 01_Basic/mathcal.js
@@ -0,0 +1,15 @@
+// Mathametics functions
+// const number = new Number (500);
+// console.log(number);
+// console.log(number);
+// console.log(number.toString());
+// console.log(number.toFixed(2));
+// console.log(number.toPrecision(1));
+// console.log(number.valueOf(2));
+// console.log(number.toExponential(10));
+
+// ------------------------------------------------Math----------------------------------------
+console.log(Math.floor(Math.random()* 10)+1);
+const min=10
+const max=20
+console.log(Math.floor(Math.random()* (max-min +1))+min);
